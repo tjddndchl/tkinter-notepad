4ef59729b1940c86fed4ca4e74b77eee53984737
DESKTOP-JGDJR1B\202-4321 <jhe0802@daum.net>
2023-07-05 12:09:01 +0900

    notepad template

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+window = Tk()
+window.title("Notepad")
+window.geometry("400 x 400")
+window.resizable(False, False)
+
+menu = Menu(window)
+menu_1 = Menu(menu, tearoff=0)
+menu_1.add_command(label="새파일")
+menu_1.add_command(label="저장")
+menu_1.add_separator()
+menu_1.add_command(label="종료", command=window.destroy)
+menu.add_cascade(label="만든이", menu=menu_2)
+
+window.config(menu=menu)
+window.mainloop()
