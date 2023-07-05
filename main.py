fa7aafe82903bd724a9e98156e22d13685b5c311
DESKTOP-JGDJR1B\202-4321 <jhe0802@daum.net>
2023-07-05 12:21:04 +0900

    Add textarea

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter.filedialog import *
 
 window = Tk()
 window.title("Notepad")
-window.geometry("400 x 400")
+window.geometry("400x400")
 window.resizable(False, False)
 
 menu = Menu(window)
@@ -12,7 +12,16 @@ menu_1.add_command(label="새파일")
 menu_1.add_command(label="저장")
 menu_1.add_separator()
 menu_1.add_command(label="종료", command=window.destroy)
+menu.add_cascade(label="만든이", menu=menu_1)
+
+menu_2 = Menu(menu, tearoff = 0)
+menu_2.add_command(label="만든이")
 menu.add_cascade(label="만든이", menu=menu_2)
+
+text_area = Text(window)
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+text_area.grid(sticky= N + E + S + W)
 
 window.config(menu=menu)
 window.mainloop()
